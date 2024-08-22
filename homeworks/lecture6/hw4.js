bc8968de152b9b73742ff2c16d71cdff089e341f
Zhiyuan Jiang <zhiyuanj@andrew.cmu.edu>
2024-08-22 16:52:13 -0400

    update hw6

--- homeworks/lecture6/hw4.js
+++ homeworks/lecture6/hw4.js
@@ -5,8 +5,26 @@
  * @param {number} num
  */
 function format(num) {
-  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  const [integerPart, fractionalPart] = num.toString().split(".");
+  let result = "";
+  let count = 0;
+  for (let i = integerPart.length - 1; i >= 0; i--) {
+    result = integerPart[i] + result;
+    count++;
+    if (count % 3 === 0 && i !== 0) {
+      result = "," + result;
+    }
+  }
+  if (fractionalPart) {
+    result += "." + fractionalPart;
+  }
+  return result;
 }
+
+// 2. Regex approach
+// function format(num) {
+//   return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+// }
 
 console.log(format(12345678));
 console.log(format(1234.56));
